cbca266c170288a1b9c95743163a381ea0170274
Fernanda Matias <fernandasmatias@gmail.com>
2018-05-28 17:56:50 -0500

    feat: Added event to show clicked cards. Only 2 cards can be opened at the same time.

--- fend-project-memory-game/js/app.js
+++ fend-project-memory-game/js/app.js
@@ -2,7 +2,10 @@
  * Create a list that holds all of your cards
  */
 const cards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
-    'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
+    'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'
+];
+
+let openedCards = [];
 
 /*
  * Display the cards on the page
@@ -11,14 +14,16 @@ const cards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
  *   - add each card's HTML to the page
  */
 const refreshButton = document.getElementsByClassName('restart');
-const deck = document.getElementsByClassName('deck');
+const deck = document.getElementById('cards-deck');
 
+//TODO: improve performance!
 function createShuffleCards(parentElement) {
     const array = shuffle(cards.concat(cards));
+    console.log('new array:' + array);
 
     const existingCards = parentElement.querySelectorAll('li');
-    if(existingCards.length > 0) {
-        for(index=0; index< existingCards.length; index++) {
+    if (existingCards.length > 0) {
+        for (index = 0; index < existingCards.length; index++) {
             (parentElement.querySelector('li')).remove();
         }
     }
@@ -27,23 +32,25 @@ function createShuffleCards(parentElement) {
         const li = document.createElement('li');
         li.className = 'card';
 
-        const i = document.createElement('i');       
+        const i = document.createElement('i');
         i.className = 'fa ' + element;
         li.appendChild(i);
         parentElement.appendChild(li);
     });
+    openedCards = [];
 }
 
-refreshButton[0].addEventListener('click', function(){
+refreshButton[0].addEventListener('click', function () {
     console.log('refresh button was clicked');
-    createShuffleCards(deck[0]);
+    createShuffleCards(deck);
 });
 
-createShuffleCards(deck[0]);
+createShuffleCards(deck);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -56,7 +63,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -67,3 +73,21 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+deck.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'LI') {
+        console.log('card clicked');
+        console.log('openedCards: ' + openedCards);
+
+        if (event.target.className === 'card' && openedCards.length < 2) {
+            event.target.className = 'card open show';
+            
+            const cardSymbol = event.target.firstChild.className;
+            openedCards.push(cardSymbol);
+
+            console.log('openedCards: ' + openedCards);
+        } else if (openedCards.length == 2) {
+            //TODO: verify selected cards
+        }
+    }
+});
