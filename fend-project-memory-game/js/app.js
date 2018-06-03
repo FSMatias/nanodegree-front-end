01f5d99275102d99e2025d238ac1f66d850e372c
Fernanda Matias <fernandasmatias@gmail.com>
2018-06-03 18:49:39 -0500

    Updated readme and clean up unnecessary comments

--- fend-project-memory-game/js/app.js
+++ fend-project-memory-game/js/app.js
@@ -1,5 +1,5 @@
 /*
- * Create a list that holds all of your cards
+ * List of all cards
  */
 const cards = ['fa-sun', 'fa-paper-plane', 'fa-anchor', 'fa-bolt',
     'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'
@@ -20,12 +20,6 @@ const regularStarClass = 'far fa-star';
 const boldStarClass = 'fa fa-star';
 const redoClass = 'fa fa-redo';
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 const refreshButton = document.getElementsByClassName('restart');
 const deck = document.getElementById('cards-deck');
 const starsList = document.getElementsByClassName('fa-star');
@@ -79,18 +73,6 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 deck.addEventListener('click', function (event) {
     if (event.target.nodeName === 'LI') {
