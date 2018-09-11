eeda73cb4c8c90af4ee5d2ad7776b64b1361c102
Fernanda Matias <fernandasmatias@gmail.com>
2018-09-11 14:52:25 -0500

    Refactored code creating component Book and Bookshelf. Temporarily moved data to App state as a static data

---
+++ reactnd-project-myreads-starter/src/Bookshelf.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+class Bookshelf extends React.Component {
+    
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+     }
+
+    render() {
+        return(
+            <div className="bookshelf">
+            <h2 className="bookshelf-title">{this.props.bookshelfTitle}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {this.props.books.map((book) => (
+                        <li key={book.title}>
+                            <Book bookInfo={book} />    
+                        </li>
+                    ))}
+                </ol>
+            </div>
+            </div>                
+        )
+    }
+}
+
+export default Bookshelf
